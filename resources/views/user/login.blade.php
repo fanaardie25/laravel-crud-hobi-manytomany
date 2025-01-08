5e247909ee05eb11bf2c6868b9a817dd940e6cb4
fanaardie25 <follgrowthindonesia@gmail.com>
2025-01-08 21:13:22 +0700

    memperbaiki tulisan yang salah di view login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -18,15 +18,15 @@
                     @csrf
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" required placeholder="Enter your email">
+                        <input type="email" class="form-control" id="email" name="email" required placeholder="Masukan Email mu">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required placeholder="Enter your password">
+                        <input type="password" class="form-control" id="password" name="password" required placeholder="Masukan Password mu">
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Login</button>
                     <p class="text-center mt-3">
-                        Don't have an account? <a href="{{ route('register') }}">Register here</a>
+                        belum punya akun? <a href="{{ route('register') }}">Registrasi disini</a>
                     </p>
                 </form>
             </div>
